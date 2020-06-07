5fa63405a448c576970b006d49cfd18ffb1e357c
Andrew Alpashko <alpashko14@gmail.com>
2020-06-07 11:53:48 +0500

    Add project files.

---
+++ TVTransformerTests/UITestFixture.cs
@@ -0,0 +1,32 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace TVTransformerTests
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            AtataContext.Configure().
+                //UseChrome().
+                //    WithArguments("start-maximized").
+                //UseBaseUrl("SITE_URL").
+                UseCulture("en-us").
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                LogNUnitError().
+                UseAssertionExceptionType<NUnit.Framework.AssertionException>().
+                UseNUnitAggregateAssertionStrategy().
+                Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+    }
+}
